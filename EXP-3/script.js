253b420f17af3f310540cdbc47110d0a909b4d16
Tamanna Samantaray <samantaraytamanna29@gmail.com>
2023-03-17 01:45:48 +0530

    main

---
+++ EXP-3/script.js
@@ -0,0 +1,70 @@
+function thpostlabsubmit(){
+    if(document.getElementById("hwqs1_op1").checked == true){
+        document.getElementById("hwans1").style.display = "block";
+        document.getElementById("hwans1").style.color = "red";
+        document.getElementById("hwans1").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs1_op2").checked == true){
+        document.getElementById("hwans1").style.display = "block";
+        document.getElementById("hwans1").style.color = "red";
+        document.getElementById("hwans1").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs1_op3").checked == true){
+        document.getElementById("hwans1").style.display = "block";
+        document.getElementById("hwans1").style.color = "green";
+        document.getElementById("hwans1").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs1_op4").checked == true){
+        document.getElementById("hwans1").style.display = "block";
+        document.getElementById("hwans1").style.color = "red";
+        document.getElementById("hwans1").style.border = "0.066vw solid black"
+
+    }
+
+
+    if(document.getElementById("hwqs2_op1").checked == true){
+        document.getElementById("hwans2").style.display = "block";
+        document.getElementById("hwans2").style.color = "red";
+        document.getElementById("hwans2").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs2_op2").checked == true){
+        document.getElementById("hwans2").style.display = "block";
+        document.getElementById("hwans2").style.color = "green";
+        document.getElementById("hwans2").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs2_op3").checked == true){
+        document.getElementById("hwans2").style.display = "block";
+        document.getElementById("hwans2").style.color = "red";
+        document.getElementById("hwans2").style.border = "0.066vw solid black"
+
+    }
+    if(document.getElementById("hwqs2_op4").checked == true){
+        document.getElementById("hwans2").style.display = "block";
+        document.getElementById("hwans2").style.color = "red";
+        document.getElementById("hwans2").style.border = "0.066vw solid black"
+
+    }
+
+
+    document.getElementById("hwans4").style.display = "block";
+    document.getElementById("hwans4").style.color = "green";
+    document.getElementById("hwans4").style.border = "0.066vw solid black"
+
+    document.getElementById("hwans5").style.display = "block";
+    document.getElementById("hwans5").style.color = "green";
+    document.getElementById("hwans5").style.border = "0.066vw solid black"
+
+
+    document.getElementById("thans3").style.display = "block";
+    document.getElementById("thans3").innerHTML = "Ans: The depended sources are left intact because they are controlled by other circuit variable."
+    document.getElementById("thans3").style.color = "green";
+
+
+
+    }
+    
